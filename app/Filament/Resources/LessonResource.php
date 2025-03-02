62cbab75757520088fc0781413cc9491028a7eb0
Boy Sitorus <sitorusboy0123@gmail.com>
2025-03-02 22:41:01 +0700

    field exp in Lesson Resource

--- app/Filament/Resources/LessonResource.php
+++ app/Filament/Resources/LessonResource.php
@@ -43,12 +43,14 @@ class LessonResource extends Resource
                     ->required()
                     ->maxLength(255)
                     ->columnSpanFull(),
-                TextInput::make('exp')
+
+                Forms\Components\TextInput::make('exp')
                     ->label('Reward Exp')
                     ->required()
                     ->columnSpanFull()
                     ->integer()
                     ->default(10),
+
                 RichEditor::make('content')
                     ->required()
                     ->fileAttachmentsDirectory('lessons/images')
