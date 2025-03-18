9ffb95f51f315e8e6b53d16640dce8ca756bab47
Boy Sitorus <sitorusboy0123@gmail.com>
2025-03-18 22:48:53 +0700

    migrations

---
+++ database/migrations/2025_03_18_200838_create_avatars_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('avatars', function (Blueprint $table) {
+            $table->id();
+            $table->string("title");
+            $table->longText("picture");
+            $table->bigInteger("price");
+            $table->unsignedBigInteger("created_by");
+            $table->unsignedBigInteger("updated_by");
+            $table->timestamp("deleted_at")->nullable();
+            $table->timestamps();
+
+            $table->foreign('created_by')->references('id')->on('admins')->onDelete('cascade');
+            $table->foreign('updated_by')->references('id')->on('admins')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('avatars');
+    }
+};
