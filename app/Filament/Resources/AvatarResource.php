14e7d3c6130d7f8c3cbc9cab997af3b9aeb619dd
Boy Sitorus <sitorusboy0123@gmail.com>
2025-03-19 19:46:08 +0700

    model avatar

---
+++ app/Filament/Resources/AvatarResource.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\AvatarResource\Pages;
+use App\Filament\Resources\AvatarResource\RelationManagers;
+use App\Models\Avatar;
+use Filament\Forms;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Columns\ImageColumn;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Illuminate\Support\Facades\Storage;
+
+class AvatarResource extends Resource
+{
+    protected static ?string $model = Avatar::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-shopping-cart';
+
+    protected static ?string $navigationGroup = 'E-Learning';
+
+    protected static ?int $navigationSort = 6;
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('title')
+                    ->label('Title')
+                    ->required()
+                    ->columnSpanFull(),
+
+                TextInput::make('price')
+                    ->label("Item's Price")
+                    ->required()
+                    ->integer()
+                    ->default(0),
+
+                FileUpload::make('picture')
+                    ->label('Picture')
+                    ->image()
+                    ->required()
+                    ->directory('avatars/images')
+                    ->getUploadedFileNameForStorageUsing(fn($file) => $file->hashName())
+                    ->columnSpanFull(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('title')
+                    ->label("Title"),
+                Tables\Columns\TextColumn::make('price')
+                    ->label("Price"),
+                ImageColumn::make('picture')
+                    ->label('Picture')
+                    ->size(50)
+                    ->url(fn($record) => Storage::url($record->picture)),
+                Tables\Columns\TextColumn::make('creator.name')
+                    ->label("Created By")
+                    ->formatStateUsing(fn($record) => $record->creator->name . "<br/> <hr/>" . $record->created_at)
+                    ->html(),
+                Tables\Columns\TextColumn::make('updater.name')
+                    ->label("Updated By")
+                    ->formatStateUsing(fn($record) => $record->updater->name . "<br/> <hr/>" . $record->updated_at)
+                    ->html(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListAvatars::route('/'),
+            'create' => Pages\CreateAvatar::route('/create'),
+            'edit' => Pages\EditAvatar::route('/{record}/edit'),
+        ];
+    }
+}
--- app/Models/Avatar.php
+++ app/Models/Avatar.php
@@ -4,8 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Avatar extends Model
 {
     use HasFactory;
+
+    public function creator(): BelongsTo
+    {
+        return $this->belongsTo(Admin::class, "created_by");
+    }
+
+    public function updater(): BelongsTo
+    {
+        return $this->belongsTo(Admin::class, "updated_by");
+    }
 }
