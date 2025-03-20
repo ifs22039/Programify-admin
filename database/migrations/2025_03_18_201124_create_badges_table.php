1dd03dcd4d9d0ce8a0b59ebb54c43f145028d158
Enrico Hezkiel Sirait <ifs21034@students.del.ac.id>
2025-03-20 08:18:32 +0700

    feat: badges

--- database/migrations/2025_03_18_201124_create_badges_table.php
+++ database/migrations/2025_03_18_201124_create_badges_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('badges', function (Blueprint $table) {
             $table->id();
+            $table->string("title");
+            $table->string("description");
+            $table->string("picture");
+            $table->timestamp("deleted_at")->nullable();
             $table->timestamps();
         });
     }
--- database/migrations/2025_03_18_233235_create_have_achievements_table.php
+++ database/migrations/2025_03_18_233235_create_have_achievements_table.php
@@ -4,20 +4,21 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+return new class extends Migration
+{
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('have_badges', function (Blueprint $table) {
+        Schema::create('have_achievements', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger("badge_id");
+            $table->unsignedBigInteger("achievement_id");
             $table->unsignedBigInteger("user_id");
             $table->timestamp("deleted_at")->nullable();
             $table->timestamps();
 
-            $table->foreign('badge_id')->references('id')->on('badges')->onDelete('cascade');
+            $table->foreign('achievement_id')->references('id')->on('achievements')->onDelete('cascade');
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
     }
@@ -27,6 +28,6 @@ return new class extends Migration {
      */
     public function down(): void
     {
-        Schema::dropIfExists('have_badges');
+        Schema::dropIfExists('have_achievements');
     }
 };
